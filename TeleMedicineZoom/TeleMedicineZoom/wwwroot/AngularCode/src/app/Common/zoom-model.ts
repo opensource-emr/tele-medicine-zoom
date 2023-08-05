0651d9478b32e63bc678ebe141307f9fcb761661
removedporn <86824510+removedporn@users.noreply.github.com>
2023-08-05 18:14:50 +0800

    Delete TeleMedicineZoom directory

--- TeleMedicineZoom/TeleMedicineZoom/wwwroot/AngularCode/src/app/Common/zoom-model.ts
+++
@@ -1,8 +0,0 @@
-export class ZoomSettingsModel {
-    signature:string;
-    meetingId:string; 
-    apiKey:string;
-    password:string;
-    leaveUrl:string;
-    role:string;
-}
--- TeleMedicineZoom/TeleMedicineZoom/wwwroot/AngularCode/src/app/Patient/patient.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit, Inject } from '@angular/core';
-
-
-@Component({
-  templateUrl: './patient.component.html',
-  styleUrls: ['./patient.component.css']
-})
-export class PatientComponent { 
-}
